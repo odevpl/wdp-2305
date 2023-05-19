b333effa6d3f2510a33690a24ccc692485026b3e
Mateusz <myszke93@gmail.com>
2023-05-19 16:19:18 +0200

    Add responisve to newFurniture

--- src/components/features/NewFurniture/NewFurniture.js
+++ src/components/features/NewFurniture/NewFurniture.js
@@ -75,7 +75,7 @@ class NewFurniture extends React.Component {
             } row`}
           >
             {categoryProducts.slice(activePage * 8, (activePage + 1) * 8).map(item => (
-              <div key={item.id} className='col-3'>
+              <div key={item.id} className='col-12 col-sm-4 col-lg-3'>
                 <ProductBox {...item} />
               </div>
             ))}
