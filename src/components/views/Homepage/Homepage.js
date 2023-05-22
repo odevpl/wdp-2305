4e3d8dfbb74bb24a18292f378952b89db41cf8d8
Grzegorz Staszczak <grzestamail@gmail.com>
2023-05-22 18:26:00 +0200

    Add basic layout & brandsRedux for data in BrandsList

--- src/components/views/Homepage/Homepage.js
+++ src/components/views/Homepage/Homepage.js
@@ -5,11 +5,13 @@ import styles from './Homepage.module.scss';
 
 import FeatureBoxes from '../../features/FeatureBoxes/FeatureBoxes';
 import NewFurniture from '../../features/NewFurniture/NewFurnitureContainer';
+import BrandsList from '../BrandsList/BrandsList';
 
 const Homepage = () => (
   <div className={styles.root}>
     <FeatureBoxes />
     <NewFurniture />
+    <BrandsList />
   </div>
 );
 
