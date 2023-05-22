dc29bfb1c7e98977d6ec4846358895008a96e5fc
Slawek Derabin <slawomirderabin@gmail.com>
2023-05-22 18:27:38 +0200

    WDP230502-20 check screen width & handle elements

--- src/components/features/NewFurniture/NewFurniture.js
+++ src/components/features/NewFurniture/NewFurniture.js
@@ -1,93 +1,108 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 import styles from './NewFurniture.module.scss';
 import ProductBox from '../../common/ProductBox/ProductBox';
 
-class NewFurniture extends React.Component {
-  state = {
-    activePage: 0,
-    activeCategory: 'bed',
-    fade: false,
+const NewFurniture = ({ categories, products }) => {
+  const [viewportWidth, setViewportWidth] = useState(window.innerWidth);
+  const [itemsPerRow, setItemsPerRow] = useState(4);
+  const [activePage, setActivePage] = useState(0);
+  const [activeCategory, setActiveCategory] = useState('bed');
+
+  const handleResize = () => {
+    setViewportWidth(window.innerWidth);
   };
 
-  handlePageChange(newPage) {
-    this.setState({ fade: true });
-    this.setState({ activePage: newPage, fade: false });
-  }
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
 
-  handleCategoryChange(newCategory) {
-    this.setState({ fade: true });
-    this.setState({ activeCategory: newCategory, fade: false, activePage: 0 });
-  }
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
-  render() {
-    const { categories, products } = this.props;
-    const { activeCategory, activePage } = this.state;
-
-    const categoryProducts = products.filter(item => item.category === activeCategory);
-    const pagesCount = Math.ceil(categoryProducts.length / 8);
-
-    const dots = [];
-    for (let i = 0; i < pagesCount; i++) {
-      dots.push(
-        <li>
-          <a
-            onClick={() => this.handlePageChange(i)}
-            className={i === activePage && styles.active}
-          >
-            page {i}
-          </a>
-        </li>
-      );
+  useEffect(() => {
+    if (viewportWidth >= 1230) {
+      setItemsPerRow(4);
+    } else if (viewportWidth >= 992) {
+      setItemsPerRow(3);
+    } else if (viewportWidth >= 767) {
+      setItemsPerRow(2);
     }
+  }, [viewportWidth]);
 
-    return (
-      <div className={styles.root}>
-        <div className='container'>
-          <div className={styles.panelBar}>
-            <div className='row no-gutters align-items-end'>
-              <div className={'col-auto ' + styles.heading}>
-                <h3>New furniture</h3>
-              </div>
-              <div className={'col ' + styles.menu}>
-                <ul>
-                  {categories.map(item => (
-                    <li key={item.id}>
-                      <a
-                        className={item.id === activeCategory && styles.active}
-                        onClick={() => this.handleCategoryChange(item.id)}
-                      >
-                        {item.name}
-                      </a>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-              <div className={'col-auto ' + styles.dots}>
-                <ul>{dots}</ul>
-              </div>
+  const handlePageChange = newPage => {
+    setActivePage(newPage);
+  };
+
+  const handleCategoryChange = newCategory => {
+    setActiveCategory(newCategory);
+    setActivePage(0);
+  };
+
+  const categoryProducts = products.filter(item => item.category === activeCategory);
+  const pagesCount = Math.ceil(categoryProducts.length / (itemsPerRow * 2));
+
+  const dots = [];
+  for (let i = 0; i < pagesCount; i++) {
+    dots.push(
+      <li key={i}>
+        <a
+          onClick={() => handlePageChange(i)}
+          className={i === activePage ? styles.active : ''}
+        >
+          page {i}
+        </a>
+      </li>
+    );
+  }
+
+  return (
+    <div className={styles.root}>
+      <div className='container'>
+        <div className={styles.panelBar}>
+          <div className='row no-gutters align-items-end'>
+            <div className={`col-auto ${styles.heading}`}>
+              <h3>New furniture</h3>
+            </div>
+            <div className={`col ${styles.menu}`}>
+              <ul>
+                {categories.map(item => (
+                  <li key={item.id}>
+                    <a
+                      className={item.id === activeCategory ? styles.active : ''}
+                      onClick={() => handleCategoryChange(item.id)}
+                    >
+                      {item.name}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div className={`col-auto ${styles.dots}`}>
+              <ul>{dots}</ul>
             </div>
           </div>
-          <div
-            className={`${this.state.fade ? styles['fade-out'] : ''} ${
-              styles['products-view']
-            } row`}
-          >
-            {categoryProducts.slice(activePage * 8, (activePage + 1) * 8).map(item => (
-              <div key={item.id} className='col-3'>
-                <ProductBox {...item} />
-              </div>
-            ))}
+          <div className={`${styles['products-view']} row`}>
+            {categoryProducts
+              .slice(
+                activePage * (itemsPerRow * 2),
+                (activePage + 1) * (itemsPerRow * 2)
+              )
+              .map(item => (
+                <div key={item.id} className={`col-${12 / itemsPerRow}`}>
+                  <ProductBox {...item} />
+                </div>
+              ))}
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 NewFurniture.propTypes = {
-  children: PropTypes.node,
   categories: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.string,
@@ -101,6 +116,7 @@ NewFurniture.propTypes = {
       category: PropTypes.string,
       price: PropTypes.number,
       stars: PropTypes.number,
+
       promo: PropTypes.string,
       newFurniture: PropTypes.bool,
     })
--- src/components/layout/MainLayout/MainLayout.js
+++ src/components/layout/MainLayout/MainLayout.js
@@ -1,16 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 
-const MainLayout = ({ children }) => (
-  <div>
-    <Header />
-    {children}
-    <Footer />
-  </div>
-);
+const MainLayout = ({ children }) => {
+  const [viewportWidth, setViewportWidth] = useState(window.innerWidth);
+
+  const handleResize = () => {
+    setViewportWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const getItemsPerRow = () => {
+    if (viewportWidth >= 1200) {
+      return 4;
+    } else if (viewportWidth >= 992) {
+      return 3;
+    } else {
+      return 2;
+    }
+  };
+
+  const itemsPerRow = getItemsPerRow();
+
+  return (
+    <div>
+      <Header />
+      <div className={`container-${itemsPerRow}`}>{children}</div>
+      <Footer />
+    </div>
+  );
+};
 
 MainLayout.propTypes = {
   children: PropTypes.node,
