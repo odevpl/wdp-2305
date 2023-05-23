33ef003e91a068536b0c127d73df3692bb2dd82b
MateuszMyszke <myszke93@gmail.com>
2023-05-23 23:00:25 +0200

    Add star ratings

--- src/redux/productsRedux.js
+++ src/redux/productsRedux.js
@@ -5,9 +5,26 @@ export const getCount = ({ products }) => products.length;
 export const getNew = ({ products }) =>
   products.filter(item => item.newFurniture === true);
 
+const createActionName = actionName => `app/products/${actionName}`;
+const UPDATE_PRODUCT_RATE = createActionName('UPDATE_PRODUCT_RATE');
+
+export const updateProductRate = payload => ({
+  type: UPDATE_PRODUCT_RATE,
+  payload,
+});
+
 /* reducer */
 export default function reducer(statePart = [], action = {}) {
   switch (action.type) {
+    case UPDATE_PRODUCT_RATE:
+      return statePart.map(product =>
+        product.id === action.payload.id
+          ? {
+              ...product,
+              userStars: action.payload.userStars,
+            }
+          : product
+      );
     default:
       return statePart;
   }
