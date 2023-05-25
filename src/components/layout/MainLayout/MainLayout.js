9aa793084da4c8d5259612599ee5b603c482a62f
MateuszMyszke <myszke93@gmail.com>
2023-05-25 20:29:39 +0200

    Add chatBot

--- src/components/layout/MainLayout/MainLayout.js
+++ src/components/layout/MainLayout/MainLayout.js
@@ -3,11 +3,13 @@ import PropTypes from 'prop-types';
 
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
+import ChatBot from '../../common/ChatBot/ChatBot';
 
 const MainLayout = ({ children }) => (
   <div>
     <Header />
     {children}
+    <ChatBot />
     <Footer />
   </div>
 );
