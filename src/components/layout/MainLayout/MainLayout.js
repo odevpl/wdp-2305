21c615e04a1ab679e0da3ec268d8179fc69c11c6
Slawek Derabin <slawomirderabin@gmail.com>
2023-05-25 16:20:38 +0200

    Feedback komponent

--- src/components/layout/MainLayout/MainLayout.js
+++ src/components/layout/MainLayout/MainLayout.js
@@ -3,11 +3,13 @@ import PropTypes from 'prop-types';
 
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
+import Feedback from '../Feedback/Feedback';
 
 const MainLayout = ({ children }) => (
   <div>
     <Header />
     {children}
+    <Feedback />
     <Footer />
   </div>
 );
