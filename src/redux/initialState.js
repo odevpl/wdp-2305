8232dfe91b9d0225c879f2c405f87446b674157f
Jakub Czesak <jakub.czesak@cbre.com>
2023-05-31 22:25:43 +0200

    Fix Blog styles

--- src/redux/initialState.js
+++ src/redux/initialState.js
@@ -369,7 +369,7 @@ const initialState = {
       title: 'Witamy na nowej stronie sklepu!',
       image: '/images/blog/blog_1.jpg',
       description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vulputate dignissim porta. Phasellus eget augue at magna fringilla feugiat. Duis suscipit tellus at velit egestas pulvinar. Pellentesque scelerisque ligula ac diam mattis, egestas imperdiet ante mattis. Quisque tortor sapien, suscipit non augue elementum, consectetur pulvinar ex. Aliquam id mi non enim gravida iaculis. Cras et cursus dui. Donec in suscipit augue, vitae bibendum urna. Curabitur vitae nunc congue, dignissim tellus a, consectetur est. Nulla molestie tempor magna, a ',
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vulputate dignissim porta. Phasellus eget augue at magna fringilla feugiat. Duis suscipit tellus at velit egestas pulvinar. Pellentesque scelerisque ligula ac diam mattis, egestas imperdiet ante mattis. Quisque tortor sapien, suscipit non augue elementum, consectetur pulvinar ex.',
     },
   ],
 };
